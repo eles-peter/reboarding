5e42cd8312207cd18dfa32c81edaaade0d61a8e9
Danko Balazs <danko.balazs95@gmail.com>
2020-06-23 07:29:45 +0200

    Create seat-coordinates.json Create RoomParser.java example

--- src/main/java/hu/csapatnev/accentureonepre/dto/Payload.java
+++ src/main/java/hu/csapatnev/accentureonepre/dto/Payload.java
@@ -1,11 +1,13 @@
 package hu.csapatnev.accentureonepre.dto;
 
 import com.fasterxml.jackson.annotation.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = Access.class, name = "access"),
         @JsonSubTypes.Type(value = Status.class, name = "status"),
 })
+@Schema(oneOf = {Access.class, Status.class})
 public interface Payload {
 }
