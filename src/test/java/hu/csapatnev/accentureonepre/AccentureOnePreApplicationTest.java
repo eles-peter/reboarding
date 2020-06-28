8195af6cbe5a1da17498e6d18d21c51003dcb069
Pepa <eles.peter.12@gmail.com>
2020-06-28 12:19:58 +0200

    corrected AccentureOnePreApplicationTest

--- src/test/java/hu/csapatnev/accentureonepre/AccentureOnePreApplicationTest.java
+++ src/test/java/hu/csapatnev/accentureonepre/AccentureOnePreApplicationTest.java
@@ -9,45 +9,48 @@ class AccentureOnePreApplicationTest {
 
     private AccentureOnePreApplication accentureOnePreApplication;
 
-//
-//    @Test
-//    void TestvalidateOrThrowStepsDate_WithValidArguments() {
-//        LocalDate stepTo10 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo20 = LocalDate.of(2020,5,31);
-//        LocalDate stepTo30 = LocalDate.of(2020,6,15);
-//        LocalDate stepTo50 = LocalDate.of(2020,6,30);
-//        LocalDate stepTo100 = LocalDate.of(2020,7,15);
-//
-//        Assertions.assertDoesNotThrow(() -> {
-//            AccentureOnePreApplication.validateOrThrowStepsDate(stepTo10, stepTo20, stepTo30, stepTo50, stepTo100);
-//        });
-//    }
-//
-//    @Test
-//    void TestvalidateOrThrowStepsDate_WithEqualsDates() {
-//        LocalDate stepTo10 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo20 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo30 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo50 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo100 = LocalDate.of(2020,5,15);
-//
-//        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-//            AccentureOnePreApplication.validateOrThrowStepsDate(stepTo10, stepTo20, stepTo30, stepTo50, stepTo100);
-//        });
-//    }
-//
-//    @Test
-//    void TestvalidateOrThrowStepsDate_WithInvalidOrder() {
-//        LocalDate stepTo10 = LocalDate.of(2020,5,15);
-//        LocalDate stepTo20 = LocalDate.of(2020,5,31);
-//        LocalDate stepTo30 = LocalDate.of(2020,6,15);
-//        LocalDate stepTo50 = LocalDate.of(2020,4,30);
-//        LocalDate stepTo100 = LocalDate.of(2020,7,15);
-//
-//        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-//            accentureOnePreApplication.validateOrThrowStepsDate(stepTo10, stepTo20, stepTo30, stepTo50, stepTo100);
-//        });
-//    }
+
+    @Test
+    void TestvalidateOrThrowStepsDate_WithValidArguments() {
+        LocalDate stepTo5 = LocalDate.of(2020,5,15);
+        LocalDate stepTo4 = LocalDate.of(2020,5,31);
+        LocalDate stepTo3 = LocalDate.of(2020,6,15);
+        LocalDate stepTo2 = LocalDate.of(2020,6,30);
+        LocalDate stepTo1 = LocalDate.of(2020,7,15);
+        LocalDate endOfPeriod = LocalDate.of(2020,7,30);
+
+        Assertions.assertDoesNotThrow(() -> {
+            AccentureOnePreApplication.validateOrThrowStepsDate(stepTo5, stepTo4, stepTo3, stepTo2, stepTo1, endOfPeriod);
+        });
+    }
+
+    @Test
+    void TestvalidateOrThrowStepsDate_WithEqualsDates() {
+        LocalDate stepTo5 = LocalDate.of(2020,5,15);
+        LocalDate stepTo4 = LocalDate.of(2020,5,15);
+        LocalDate stepTo3 = LocalDate.of(2020,5,15);
+        LocalDate stepTo2 = LocalDate.of(2020,5,15);
+        LocalDate stepTo1 = LocalDate.of(2020,5,15);
+        LocalDate endOfPeriod = LocalDate.of(2020,5,15);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            AccentureOnePreApplication.validateOrThrowStepsDate(stepTo5, stepTo4, stepTo3, stepTo2, stepTo1, endOfPeriod);
+        });
+    }
+
+    @Test
+    void TestvalidateOrThrowStepsDate_WithInvalidOrder() {
+        LocalDate stepTo5 = LocalDate.of(2020,5,15);
+        LocalDate stepTo4 = LocalDate.of(2020,5,31);
+        LocalDate stepTo3 = LocalDate.of(2020,6,15);
+        LocalDate stepTo2 = LocalDate.of(2020,4,30);
+        LocalDate stepTo1 = LocalDate.of(2020,7,15);
+        LocalDate endOfPeriod = LocalDate.of(2020,7,10);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            accentureOnePreApplication.validateOrThrowStepsDate(stepTo5, stepTo4, stepTo3, stepTo2, stepTo1, endOfPeriod);
+        });
+    }
 
 
 
