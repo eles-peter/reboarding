cb7460c2f5ba8a7a52be4cda47a4dbabd9ee7a1d
Danko Balazs <danko.balazs95@gmail.com>
2020-06-29 18:02:33 +0200

    update postman tests

--- start-with-mock-data.sh
+++ start-with-mock-data.sh
@@ -2,8 +2,9 @@ docker run \
   -p 8080:8080 \
   -d --rm \
   --name reboarding-test reboarding \
-  --date.stepTo10="$(date '+%Y-%m-%d')" \
-  --date.stepTo20="$(date -d '+1 day' '+%Y-%m-%d')" \
-  --date.stepTo30="$(date -d '+2 day' '+%Y-%m-%d')" \
-  --date.stepTo50="$(date -d '+3 day' '+%Y-%m-%d')" \
-  --date.stepTo100="$(date -d '+4 day' '+%Y-%m-%d')"
+  --date.stepTo5="$(date '+%Y-%m-%d')" \
+  --date.stepTo4="$(date -d '+1 day' '+%Y-%m-%d')" \
+  --date.stepTo3="$(date -d '+2 day' '+%Y-%m-%d')" \
+  --date.stepTo2="$(date -d '+3 day' '+%Y-%m-%d')" \
+  --date.stepTo1="$(date -d '+4 day' '+%Y-%m-%d')" \
+  --date.endOfPeriod="$(date -d '+5 day' '+%Y-%m-%d')"
